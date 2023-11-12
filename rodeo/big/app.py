5e3cf17b31eb85e34732bc26e85cd5f9e854428d
Sourcery AI <>
2023-11-12 06:52:40 +0000

    'Refactored by Sourcery'

--- rodeo/big/app.py
+++ rodeo/big/app.py
@@ -35,8 +35,7 @@ settings = {
 def num_tokens_from_string(string: str, model_name: str) -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.encoding_for_model(model_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
+    return len(encoding.encode(string))
 
 
 SUPPORTED_FILE_TYPES = [
